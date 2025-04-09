4772c0b5ee467ef86c650313f26548d9cf8da52b
debarka <debarkamondal@gmail.com>
2025-04-09 17:57:26 +0530

    GET POST & PATCH working on /admin/products

--- lambda/admin/products.ts
+++ lambda/admin/products.ts
@@ -28,30 +28,43 @@ const getPresignedUrl = (id: string, images: string[]) => {
     return preSignedUrls
 };
 
+const genUpdateExp = (body: any) => {
+    let updateExp: string[] = [];
+    let ExpAttrVals: { [key: string]: string } = {}
+    const keys = Object.keys(body)
+    for (let i = 0; i < keys.length; i++) {
+        if (keys[i] === 'category') updateExp.push(`lsi = :category`)
+        else updateExp.push(` ${keys[i]}= :${keys[i]}`)
+        Object.assign(ExpAttrVals, { [`:${keys[i]}`]: body[keys[i]] })
+    }
+    return { UpdateExpression: updateExp.join(", "), ExpressionAttributeValues: ExpAttrVals }
+}
+
+
 app.post('/admin/products', async (c) => {
     const body: Omit<Product, "id"> = await c.req.json()
     try {
         const id = ulid();
-        const imageUrls = await Promise.all(getPresignedUrl(id, [body.thumbnail, ...body.images]))
         const result = await db.put({
             TableName,
             Item: {
                 pk: "product",
                 sk: id,
                 lsi: body.category,
+                title: body.title,
+                images: body.images,
                 thumbnail: body.thumbnail,
                 price: body.price,
-                title: body.title,
                 ratings: body.ratings,
-                images: body.images,
                 variants: body.variants,
                 info: body.info,
                 specs: body.specs
             },
         })
         if (result.$metadata.httpStatusCode !== 200) {
-            return c.json({status: "error", message:"Couldn't add product"})
+            return c.json({ status: "error", message: "DB error" })
         }
+        const imageUrls = await Promise.all(getPresignedUrl(id, [body.thumbnail, ...body.images]))
         return c.json({ imageUrls })
     } catch (error: any) {
         throw new Error(error)
@@ -68,7 +81,7 @@ app.delete('/admin/products/:id', async (c) => {
             },
             ReturnValues: 'ALL_OLD'
         })
-        if (result.$metadata.httpStatusCode !== 200) return c.json({status:"error", message: "DB error" }, 400)
+        if (result.$metadata.httpStatusCode !== 200) return c.json({ status: "error", message: "DB error" }, 400)
         const { $metadata } = await s3Client.send(
             new DeleteObjectsCommand({
                 Bucket,
@@ -77,8 +90,33 @@ app.delete('/admin/products/:id', async (c) => {
                 },
             }),
         );
-        if ($metadata.httpStatusCode !== 200) return c.json({status:"error", message: "S3 error"}, 400)
-            return c.json({status: "success", messages: "Product deleted"})
+        if ($metadata.httpStatusCode !== 200) return c.json({ status: "error", message: "S3 error" }, 400)
+        return c.json({ status: "success", messages: "Product deleted" })
+    } catch (error: any) {
+        throw new Error(error)
+    }
+})
+app.patch('/admin/products/:id', async (c) => {
+    const id = c.req.param('id').split("-")
+
+    const body = await c.req.json()
+    const { UpdateExpression, ExpressionAttributeValues } = genUpdateExp(body)
+    let imageUrls: string[] = []
+    try {
+        const result = await db.update({
+            TableName,
+            Key: {
+                pk: id[0],
+                sk: id[1]
+            },
+            UpdateExpression: "set" + " " + UpdateExpression,
+            ExpressionAttributeValues
+        }
+        )
+        if (result.$metadata.httpStatusCode !== 200) return c.json({ status: "error", message: "DB error" })
+        imageUrls = await Promise.all(getPresignedUrl(id[1], [body.thumbnail, ...body.images]))
+        return c.json({ imageUrls })
+
     } catch (error: any) {
         throw new Error(error)
     }
