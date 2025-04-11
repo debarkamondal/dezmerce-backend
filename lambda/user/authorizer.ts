a0cde92ae065dde9efd5391c2daf7c499ebe67a6
debarka <debarkamondal@gmail.com>
2025-04-11 12:28:00 +0530

    created boilerplate for /cart

---
+++ lambda/user/authorizer.ts
@@ -0,0 +1,19 @@
+import { APIGatewaySimpleAuthorizerWithContextResult, APIGatewayRequestAuthorizerEventV2 } from "aws-lambda";
+import { verify } from "hono/jwt";
+type context = {
+    role?: string
+}
+export const handler = async (event: APIGatewayRequestAuthorizerEventV2): Promise<APIGatewaySimpleAuthorizerWithContextResult<context>> => {
+    const decodedToken = await verify(event.headers?.authorization as string, process.env.JWTSecret as string)
+    if (decodedToken.role === 'user') {
+        return {
+            isAuthorized: true,
+            context: { role: decodedToken.role }
+        };
+    }
+    return {
+        isAuthorized: false,
+        context: {}
+    };
+}
+
