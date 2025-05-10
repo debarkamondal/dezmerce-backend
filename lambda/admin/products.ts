8fa845a3ca4157ff38b2ab0e5ab3068aabe07185
debarka <debarkamondal@gmail.com>
2025-05-10 09:55:07 +0530

    fix: security issue caused due to search of any user provided key when GET/categories/[category]

--- lambda/admin/products.ts
+++ lambda/admin/products.ts
@@ -8,7 +8,7 @@ import { ulid } from "ulidx";
 import { getPresignedUrl } from "../utils/lib";
 
 type Bindings = {
-    event: AuthEvent
+        event: AuthEvent
 }
 const TableName = process.env.DB_TABLE_NAME;
 const Bucket = process.env.BUCKET_NAME;
@@ -22,118 +22,119 @@ const s3Client = new S3Client({ region });
 
 
 const genUpdateExp = (body: any) => {
-    let updateExp: string[] = [];
-    let ExpAttrVals: { [key: string]: string } = {}
-    const keys = Object.keys(body)
-    for (let i = 0; i < keys.length; i++) {
-        if (keys[i] === 'category') updateExp.push(`lsi = :category`)
-        else updateExp.push(` ${keys[i]}= :${keys[i]}`)
-        Object.assign(ExpAttrVals, { [`:${keys[i]}`]: body[keys[i]] })
-    }
-    return { UpdateExpression: updateExp.join(", "), ExpressionAttributeValues: ExpAttrVals }
+        let updateExp: string[] = [];
+        let ExpAttrVals: { [key: string]: string } = {}
+        const keys = Object.keys(body)
+        for (let i = 0; i < keys.length; i++) {
+                if (keys[i] === 'category') updateExp.push(`lsi = :category`)
+                else updateExp.push(` ${keys[i]}= :${keys[i]}`)
+                Object.assign(ExpAttrVals, { [`:${keys[i]}`]: body[keys[i]] })
+        }
+        return { UpdateExpression: updateExp.join(", "), ExpressionAttributeValues: ExpAttrVals }
 }
 
 
 app.post('/admin/products', async (c) => {
-    const body: Omit<Product, "id"> = await c.req.json()
-    try {
-        const id = ulid();
-        const result = await db.put({
-            TableName,
-            Item: {
-                pk: body.category,
-                sk: id,
-                lsi: body.gender,
-                title: body.title,
-                images: body.images,
-                thumbnail: body.thumbnail,
-                price: body.price,
-                ratings: body.ratings,
-                variants: body.variants,
-                description: body.description,
-                specs: body.specs
-            },
-        })
-        const categories = await db.get({
-            TableName,
-            Key: {
-                pk: 'categories',
-                sk: 'metadata',
-            },
-            ProjectionExpression: body.category
-        })
-        if(!categories.Item) return c.json({status:"error", message: "DB error"}) 
-        const res = await db.update({
-            TableName,
-            Key: {
-                pk: 'categories',
-                sk: 'metadata'
-            },
-            UpdateExpression: `set ${body.category} = :category`,
-            ExpressionAttributeValues: {
-                ":category": {
-                    ...categories.Item[body.category],
-                    qty: categories.Item[body.category].qty + 1
-                }
-            }
-        })
-        console.log(res)
-        if (res.$metadata.httpStatusCode !== 200) return c.json({ status: "error", message: "DB error" })
-        const thumbnailUrl = await getPresignedUrl(`products/${body.category}/${id}/${body.thumbnail}`)
-        const imageUrls = body.images.map((image) => getPresignedUrl(`products/${body.category}/${id}/${image}`))
-        return c.json({ id: body.category + "-" + id, thumbnail: thumbnailUrl, imageUrls: await Promise.all(imageUrls) })
-    } catch (error: any) {
-        throw new Error(error)
-    }
+        const body: Omit<Product, "id"> = await c.req.json()
+        try {
+                const id = ulid();
+                const result = await db.put({
+                        TableName,
+                        Item: {
+                                pk: "products:" + body.category,
+                                sk: id,
+                                lsi: body.gender,
+                                title: body.title,
+                                images: body.images,
+                                thumbnail: body.thumbnail,
+                                price: body.price,
+                                ratings: body.ratings,
+                                variants: body.variants,
+                                description: body.description,
+                                specs: body.specs
+                        },
+                })
+                const categories = await db.get({
+                        TableName,
+                        Key: {
+                                pk: 'categories',
+                                sk: 'metadata',
+                        },
+                        ProjectionExpression: body.category
+                })
+                if (!categories.Item) return c.json({ status: "error", message: "DB error" })
+                const res = await db.update({
+                        TableName,
+                        Key: {
+                                pk: 'categories',
+                                sk: 'metadata'
+                        },
+                        UpdateExpression: `set ${body.category} = :category`,
+                        ExpressionAttributeValues: {
+                                ":category": {
+                                        ...categories.Item[body.category],
+                                        qty: categories.Item[body.category].qty + 1
+                                }
+                        }
+                })
+                if (res.$metadata.httpStatusCode !== 200) return c.json({ status: "error", message: "DB error" })
+                const thumbnailUrl = await getPresignedUrl(`products/${body.category}/${id}/${body.thumbnail}`)
+                const imageUrls = body.images.map((image) => getPresignedUrl(`products/${body.category}/${id}/${image}`))
+                return c.json({ id: body.category + "-" + id, thumbnail: thumbnailUrl, imageUrls: await Promise.all(imageUrls) })
+        } catch (error: any) {
+                throw new Error(error)
+        }
 })
 app.delete('/admin/products', async (c) => {
-    const { category, id } = await c.req.json()
-    try {
-        const result = await db.delete({
-            TableName,
-            Key: {
-                pk: category,
-                sk: id
-            },
-            ReturnValues: 'ALL_OLD'
-        })
-        if (result.$metadata.httpStatusCode !== 200) return c.json({ status: "error", message: "DB error" }, 400)
-        const { $metadata } = await s3Client.send(
-            new DeleteObjectsCommand({
-                Bucket,
-                Delete: {
-                    Objects: [result.Attributes?.thumbnail, ...result.Attributes?.images].map((key: string) => ({ Key: `products/${id.split("-")[0]}/${id.split("-")[1]}/${key}` })),
-                },
-            }),
-        );
-        if ($metadata.httpStatusCode !== 200) return c.json({ status: "error", message: "S3 error" }, 400)
-        return c.json({ status: "success", messages: "Product deleted" })
-    } catch (error: any) {
-        throw new Error(error)
-    }
-})
-app.patch('/admin/products', async (c) => {
-    const body = await c.req.json()
-    const { UpdateExpression, ExpressionAttributeValues } = genUpdateExp(body)
-    try {
-        const result = await db.update({
-            TableName,
-            Key: {
-                pk: "product",
-                sk: body.category + "-" + body.id
-            },
-            UpdateExpression: "set" + " " + UpdateExpression,
-            ExpressionAttributeValues
+        const { category, id } = await c.req.json()
+        try {
+                const result = await db.delete({
+                        TableName,
+                        Key: {
+                                pk: "products" + category,
+                                sk: id
+                        },
+                        ReturnValues: 'ALL_OLD'
+                })
+                if (result.$metadata.httpStatusCode !== 200) return c.json({ status: "error", message: "DB error" }, 400)
+                const { $metadata } = await s3Client.send(
+                        new DeleteObjectsCommand({
+                                Bucket,
+                                Delete: {
+                                        Objects: [result.Attributes?.thumbnail, ...result.Attributes?.images].map((key: string) => ({ Key: `products/${id.split("-")[0]}/${id.split("-")[1]}/${key}` })),
+                                },
+                        }),
+                );
+                if ($metadata.httpStatusCode !== 200) return c.json({ status: "error", message: "S3 error" }, 400)
+                return c.json({ status: "success", messages: "Product deleted" })
+        } catch (error: any) {
+                throw new Error(error)
         }
-        )
-        if (result.$metadata.httpStatusCode !== 200) return c.json({ status: "error", message: "DB error" })
-        const thumbnailUrl = await getPresignedUrl(body.id)
-        const imageUrls = body.images.map((image: string) => getPresignedUrl(body.id))
-        return c.json({ thumbnail: thumbnailUrl, imageUrls: await Promise.all(imageUrls) })
+})
 
-    } catch (error: any) {
-        throw new Error(error)
-    }
+//FIX: Wrong PK & SK
+app.patch('/admin/products', async (c) => {
+        const body = await c.req.json()
+        const { UpdateExpression, ExpressionAttributeValues } = genUpdateExp(body)
+        try {
+                const result = await db.update({
+                        TableName,
+                        Key: {
+                                pk: "product",
+                                sk: body.category + "-" + body.id
+                        },
+                        UpdateExpression: "set" + " " + UpdateExpression,
+                        ExpressionAttributeValues
+                }
+                )
+                if (result.$metadata.httpStatusCode !== 200) return c.json({ status: "error", message: "DB error" })
+                const thumbnailUrl = await getPresignedUrl(body.id)
+                const imageUrls = body.images.map((image: string) => getPresignedUrl(body.id))
+                return c.json({ thumbnail: thumbnailUrl, imageUrls: await Promise.all(imageUrls) })
+
+        } catch (error: any) {
+                throw new Error(error)
+        }
 })
 
 export const handler = handle(app)
--- lambda/user/products.ts
+++ lambda/user/products.ts
@@ -5,7 +5,7 @@ import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb'
 import { AuthEvent } from "../../types";
 
 type Bindings = {
-    event: AuthEvent
+        event: AuthEvent
 }
 
 const dbClient = new DynamoDB({})
@@ -15,47 +15,49 @@ const app = new Hono<{ Bindings: Bindings }>()
 
 
 app.get('/products/:id', async (c) => {
-    try {
-        const { Item } = await db.get({
-            TableName,
-            Key: {
-                pk: c.req.param('id').split('-')[0],
-                sk: c.req.param('id').split('-')[1]
-            },
-        })
-        if (!Item) return c.json({ status: "error", message: "No product found with given id" })
-        return c.json({
-            category: Item.pk,
-            id: Item.sk,
-            gender: Item.gender,
-            title: Item.title,
-            images: Item.images,
-            thumbnail: Item.thumbnail,
-            price: Item.price,
-            ratings: Item.ratings,
-            variants: Item.variants,
-            description: Item.description,
-            specs: Item.specs
-        })
-    } catch (error) {
-        c.status(400)
-        return c.json({ status: 'error', message: error })
-    }
+        try {
+                const { Item } = await db.get({
+                        TableName,
+                        Key: {
+                                pk: "products:" + c.req.param('id').split('-')[0],
+                                sk: c.req.param('id').split('-')[1]
+                        },
+                })
+                if (!Item) return c.json({ status: "error", message: "No product found with given id" })
+                return c.json({
+                        category: Item.pk,
+                        id: Item.sk,
+                        gender: Item.gender,
+                        title: Item.title,
+                        images: Item.images,
+                        thumbnail: Item.thumbnail,
+                        price: Item.price,
+                        ratings: Item.ratings,
+                        variants: Item.variants,
+                        description: Item.description,
+                        specs: Item.specs
+                })
+        } catch (error) {
+                c.status(400)
+                return c.json({ status: 'error', message: error })
+        }
 })
+
+//FIX:This endpoint needs to be updated.
 app.get('/products', async (c) => {
-    try {
-        const data = await db.query({
-            TableName,
-            KeyConditionExpression: 'pk = :pk',
-            ExpressionAttributeValues:{
-                ":pk": "product"
-            },
-            ProjectionExpression: "sk, thumbnail, price, title"
-        })
-        return c.json(data.Items)
-    } catch (error) {
-        c.status(400)
-        return c.json({ status: 'error', message: error })
-    }
+        try {
+                const data = await db.query({
+                        TableName,
+                        KeyConditionExpression: 'pk = :pk',
+                        ExpressionAttributeValues: {
+                                ":pk": "product"
+                        },
+                        ProjectionExpression: "sk, thumbnail, price, title"
+                })
+                return c.json(data.Items)
+        } catch (error) {
+                c.status(400)
+                return c.json({ status: 'error', message: error })
+        }
 })
 export const handler = handle(app)
