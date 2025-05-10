ec4b987919bc0d9331c39ed3ed07edee5b78ef3f
debarka <debarkamondal@gmail.com>
2025-05-10 13:51:27 +0530

    updated data structure

--- lambda/user/categories.ts
+++ lambda/user/categories.ts
@@ -4,7 +4,7 @@ import { DynamoDB } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb'
 
 type Bindings = {
-    event: LambdaEvent
+  event: LambdaEvent
 }
 
 const TableName = process.env.DB_TABLE_NAME;
@@ -15,27 +15,27 @@ const app = new Hono<{ Bindings: Bindings }>()
 
 
 app.get('/categories', async (c) => {
-    const { Item } = await db.get({
-        TableName,
-        Key: {
-            pk: "categories",
-            sk: "metadata"
-        }
-    })
-    if (!Item) return c.json({ status: 'error', message: 'no categories found' })
-    return c.json(Item)
+  const { Item } = await db.get({
+    TableName,
+    Key: {
+      pk: "categories",
+      sk: "metadata"
+    }
+  })
+  if (!Item) return c.json({ status: 'error', message: 'no categories found' })
+  return c.json(Item)
 })
 
 app.get('/categories/:category', async (c) => {
-    const { Items } = await db.query({
-        TableName,
-        KeyConditionExpression: "pk = :category",
-        ExpressionAttributeValues: {
-            ":category": c.req.param("category"),
-        },
-        ProjectionExpression: "pk, sk, thumbnail, price, title"
-    })
-    if (!Items) return c.json({ status: 'error', message: 'no categories found' })
-    return c.json(Items)
+  const { Items } = await db.query({
+    TableName,
+    KeyConditionExpression: "pk = :category",
+    ExpressionAttributeValues: {
+      ":category": "products:" + c.req.param("category"),
+    },
+    ProjectionExpression: "pk, sk, thumbnail, price, title"
+  })
+  if (!Items) return c.json({ status: 'error', message: 'no categories found' })
+  return c.json(Items)
 })
 export const handler = handle(app)
